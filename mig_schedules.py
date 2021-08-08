34805201d65eca9a2b3b9b624c692e7f853bf59c
linusExploit <moh.hsj@gmail.com>
2021-08-08 19:14:20 +0400

    -- fixing expire schedules

--- mig_schedules.py
+++ mig_schedules.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import re
 import time
+import datetime
 
 
 dgname= 'NGFW-HOME'
@@ -56,7 +57,7 @@ human_time = time.ctime(seconds)
 l_month = human_time.split(' ')[1]
 l_day = human_time.split(' ')[3]
 if (int(l_day) < 10):
-    l_day = '0' + l_day
+    l_day_zero = '0' + l_day
 l_clock = human_time.split(' ')[4]
 l_clock = l_clock[0:5]
 l_year = human_time.split(' ') [5]
@@ -69,9 +70,17 @@ for s in schedules:
     s_day = s.split('\n')[1].split(' ')[4].strip()
     s_month = s.split('\n')[1].split(' ')[5].strip()
     s_year = s.split('\n')[1].split(' ')[6].strip()
+    #excluding expired schedules
+    c_date = datetime.datetime(int(l_year), int(months_linux[l_month]), int(l_day))
+    s_date = datetime.datetime(int(s_year), int(months[s_month]), int(s_day))
+    if (c_date > s_date):
+        continue 
+
+
+
     #print(s_name)
     #print(s_month)
     #print(s_day)
     #print(s_clock)
     #print(s_year)
-    print("set device-group {} schedule {} schedule-type non-recurring {}/{}/{}@{}-{}/{}/{}@{}").format(dgname, s_name, s_year, months[s_month], s_day, s_clock, l_year, months_linux[l_month], l_day,l_clock)
+    print("set device-group {} schedule {} schedule-type non-recurring {}/{}/{}@{}-{}/{}/{}@{}").format(dgname, s_name, l_year, months_linux[l_month], l_day_zero,l_clock,s_year, months[s_month], s_day, s_clock,)
