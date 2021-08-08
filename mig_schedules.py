b0be26b6d02e0aab70fad7f5fff411bce98279b5
linusExploit <moh.hsj@gmail.com>
2021-08-08 18:39:08 +0400

    Add files via upload

---
+++ mig_schedules.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+import re
+import time
+
+
+dgname= 'NGFW-HOME'
+
+months_linux = {'Jan':'01',
+        'Feb':'02',
+        'Mar':'03',
+        'Apr':'04',
+        'May':'05',
+        'Jun':'06',
+        'Jul':'07',
+        'Aug':'08',
+        'Sep':'09',
+        'Oct':'10',
+        'Nov':'11',
+        'Dec':'12'
+        }
+
+
+months = {'January':'01',
+        'February':'02',
+        'March':'03',
+        'April':'04',
+        'May':'05',
+        'June':'06',
+        'July':'07',
+        'August':'08',
+        'September':'09',
+        'October':'10',
+        'November':'11',
+        'December':'12'
+        }
+
+
+
+f = open('schedules.txt', 'r')
+x = f.readlines()
+
+
+all_strings = ''.join(x)
+
+schedules = re.findall(r'time-range.*?!'  , all_strings,re.DOTALL)
+
+#for s in schedules:
+    #print(s)
+    #print("++++++++++++++++++++++++++")
+
+seconds = time.time()
+human_time = time.ctime(seconds)
+
+
+## Parsing the local time as a start date
+l_month = human_time.split(' ')[1]
+l_day = human_time.split(' ')[3]
+if (int(l_day) < 10):
+    l_day = '0' + l_day
+l_clock = human_time.split(' ')[4]
+l_clock = l_clock[0:5]
+l_year = human_time.split(' ') [5]
+
+
+# iterating over every schedule and parsing the data
+for s in schedules:
+    s_name = s.split('\n')[0].split(' ')[1].strip()
+    s_clock = s.split('\n')[1].split(' ')[3].strip()
+    s_day = s.split('\n')[1].split(' ')[4].strip()
+    s_month = s.split('\n')[1].split(' ')[5].strip()
+    s_year = s.split('\n')[1].split(' ')[6].strip()
+    #print(s_name)
+    #print(s_month)
+    #print(s_day)
+    #print(s_clock)
+    #print(s_year)
+    print("set device-group {} schedule {} schedule-type non-recurring {}/{}/{}@{}-{}/{}/{}@{}").format(dgname, s_name, s_year, months[s_month], s_day, s_clock, l_year, months_linux[l_month], l_day,l_clock)
